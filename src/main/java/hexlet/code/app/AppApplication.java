f3bc146b50accd4fc646351b892898bf3ae75678
AlexSekret <alexsekret3@gmail.com>
2025-05-28 20:45:08 +0300

    have no idea how name this commit

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -1,14 +1,18 @@
 package hexlet.code.app;
 
 import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class AppApplication {
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
